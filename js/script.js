fc63ce775aa6ba327f10c3af1a11c265c2f82214
Christopher Peguero <christopherp2047@queenstechhs.org>
2018-11-27 21:38:01 +0000

    final edit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,35 @@
-// *************************** YOUR CODE BELOW *******************************
-//******************TEST EARLY AND OFTEN USING console.log() ******************
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
 
-//GLOBAL VARIABLES
-/* global $ */
+var input;
+var comp=["rock","paper","scissors"];
+function game(){
+
+   var number=Math.floor(Math.random() * 3);
+   $('#userChoice').text(inputV);
+   $('#computerChoice').text(comp[number]);
+
+   if(inputV === comp[number]){
+       $('#result').text("Tie!");
+   } else if((inputV === 'rock' && comp[number] === 'scissors') ||
+   (inputV === 'paper' && comp[number] === 'rock') ||
+   (inputV === 'scissors' && comp[number] === 'paper')) {
+       $('#result').text('You win');
+   }else if((inputV === 'rock' && comp[number] === 'paper') ||
+   (inputV === 'paper' && comp[number] === 'scissors') ||
+   (inputV === 'scissors' && comp[number] === 'rock')){
+       $('#result').text('CPU Wins');
+   }
+}
+$('#shoot').click(function() {
+   inputV = $("#input").val();
+   if(inputV === ''){
+       alert("You didn't input anything");
+   }else if(inputV != 'rock' && inputV != 'paper' && inputV != 'scissors'){
+       alert("Not a valid input");
 
 
+   }else{
+       game();
+   }
 
-// DOCUMENT READY FUNCTION BELOW
 
+});
